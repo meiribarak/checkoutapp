728470355ccd21fe95e1eb886a1a6f7a3855fe53
Barak Meiri <barakm@orpak.com>
2022-12-29 23:19:28 +0200

    update ui for empty basket

--- src/components/checkout/BasketItemsList.js
+++ src/components/checkout/BasketItemsList.js
@@ -1,23 +1,40 @@
 import React from "react";
 import "../ui/css/styles.css";
 import BasketLineItem from "./BasketLineItem ";
+import BasketLineItemEmpty from "./BasketLineItemEmpty";
 
 //items: [{ code: "", name: "", img: "", quantity: ""}],
 
 function BasketItemsList(props) {
+  console.log('BasketItemsList');
+  console.log(props);
+
+  function createEmptyLines(lines) {
+    const emptyLines = [];
+    for (let index = 0; index < lines; index++) {
+      emptyLines.push(<BasketLineItemEmpty />);
+    }
+    return emptyLines.join("");
+  }
+
   return (
     <ul className="listCards">
-      {props.items.map((item) => (
-        <BasketLineItem
-          key={item.code}
-          code={item.code}
-          name={item.name}
-          img={item.img}
-          quantity={item.quantity}
-        />
-      ))}
+      {props.items && props.items.length > 0
+        ? props.items.map((item) => (
+            <BasketLineItem
+              key={item.code}
+              code={item.code}
+              name={item.name}
+              img={item.img}
+              quantity={item.quantity}
+            />
+          ))
+        : <BasketLineItemEmpty />}  
+        
     </ul>
   );
 }
 
 export default BasketItemsList;
+
+//createEmptyLines(props.emptyLines || 1)}      
---
+++ src/components/checkout/BasketLineItemEmpty.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../ui/css/styles.css";
+
+function BasketLineItemEmpty() {
+  return (
+    <li className="listCards__louding">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </li>
+  );
+}
+
+export default BasketLineItemEmpty;
