7495ecccc232038a722ebcfcbcfdd8e0123ff0e8
Barak Meiri <barakm@orpak.com>
2022-12-29 22:48:21 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout/Layout";
+import LoginPage from "./pages/LoginPage";
+import BasketPage from "./pages/BasketPage";
+import HomePage from "./pages/HomePage";
+import FeedbackPage from "./pages/FeedbackPage";
+import ThankYouPage from "./pages/ThankYouPage";
+import RestAssurePage from "./pages/RestAssurePage";
+import PhoneNumberPage from "./pages/PhoneNumberPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LoginPage />} exact={true}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/basket" element={<BasketPage />}></Route>
+          <Route path="/feedback" element={<FeedbackPage />}></Route>
+          <Route path="/thankyou" element={<ThankYouPage />}></Route>
+          <Route path="/restassure" element={<RestAssurePage />}></Route>
+          <Route path="/phonenumber" element={<PhoneNumberPage />}></Route>
+        </Routes>
+      </Layout>
     </div>
   );
 }
--- src/components/ui/CustomerHeader.js
+++ src/components/ui/CustomerHeader.js
@@ -1,17 +1,16 @@
 import "./css/styles.css";
 import customerLogo from "./images/logo.png";
 
-
 function CustomerHeader(props) {
   return (
     <div>
       <header className="header">
         <a href="#" className="logo">
-        <img src={customerLogo} alt="Logo" />
+          <img src={customerLogo} alt="Logo" />
         </a>
       </header>
     </div>
-  );  
+  );
 }
 
 export default CustomerHeader;
